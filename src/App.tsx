9abb2330f412f31ca521452bdf0b104924bf5f36
r-4bb1t <r4bb1t.github@gmail.com>
2020-10-28 04:00:51 +0900

    add: add browser router basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Main from "./view/main";
 
-import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
-
 function App() {
   return (
     <>
