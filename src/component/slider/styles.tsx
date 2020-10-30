99dac93be22af9decce162dcd88e558ddac9042e
r-4bb1t <r4bb1t.github@gmail.com>
2020-10-31 01:41:55 +0900

    fix: update slider to support moz

--- src/component/slider/styles.tsx
+++ src/component/slider/styles.tsx
@@ -38,7 +38,48 @@ export const RangeInput = styled.input<SliderProps>`
     border-bottom: 1px solid #ccc;
     cursor: pointer;
   }
+  ::-moz-range-thumb {
+    -webkit-appearance: none;
+    height: 2.4rem;
+    margin-top: -0.2rem;
+    width: 1rem;
+    background-color: #ddd;
+    border-bottom: 1px solid #ccc;
+    cursor: pointer;
+  }
   ::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 2rem;
+    background: ${(props) =>
+      props.index === "hue"
+        ? `linear-gradient(
+        90deg,
+        rgba(255, 0, 0, 1) 0%,
+        rgba(255, 154, 0, 1) 10%,
+        rgba(208, 222, 33, 1) 20%,
+        rgba(79, 220, 74, 1) 30%,
+        rgba(63, 218, 216, 1) 40%,
+        rgba(47, 201, 226, 1) 50%,
+        rgba(28, 127, 238, 1) 60%,
+        rgba(95, 21, 242, 1) 70%,
+        rgba(186, 12, 248, 1) 80%,
+        rgba(251, 7, 217, 1) 90%,
+        rgba(255, 0, 0, 1) 100%
+    )`
+        : props.index === "sat"
+        ? `linear-gradient(
+        90deg,
+        hsl(calc(${props.hue} * 3.6), 0%, 50%) 0%,
+        hsl(calc(${props.hue} * 3.6), 100%, 50%) 100%
+    )`
+        : `linear-gradient(
+        90deg,
+        hsl(calc(${props.hue} * 3.6), 100%, 0%) 0%,
+        hsl(calc(${props.hue} * 3.6), 100%, 50%) 50%,
+        hsl(calc(${props.hue} * 3.6), 100%, 100%) 100%
+    )`};
+  }
+  ::-moz-range-track {
     width: 100%;
     height: 2rem;
     background: ${(props) =>
